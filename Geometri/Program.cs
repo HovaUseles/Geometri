7f6aa6c385d15644f4fc4efc47df09f8c0a12b76
HovaUseles <bocaj.nielsen15@gmail.com>
2021-08-30 13:14:39 +0200

    Finished

--- Geometri/Program.cs
+++ Geometri/Program.cs
@@ -10,6 +10,29 @@ namespace Geometri
     {
         static void Main(string[] args)
         {
+            Random rand = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int numGenFirst = rand.Next(3, 10);
+                int numGenSecond = rand.Next(0, 9);
+                float numGenConcat = (float)numGenFirst + ((float)numGenSecond / 10);
+                Square tempBox = new Square(numGenConcat);
+                Console.WriteLine("Before:");
+                Console.WriteLine($"Box {i + 1} Side: {tempBox.Side}");
+                Console.WriteLine($"Box {i+1} Perimeter: {tempBox.Perimeter()}");
+                Console.WriteLine($"Box {i+1} Area: {tempBox.Area()}");
+                Console.WriteLine();
+                Console.WriteLine("After:");
+                tempBox.Side += rand.Next(-2, 5);
+                Console.WriteLine($"Box {i + 1} Side: {tempBox.Side}");
+                Console.WriteLine($"Box {i + 1} Perimeter: {tempBox.Perimeter()}");
+                Console.WriteLine($"Box {i + 1} Area: {tempBox.Area()}");
+                Console.WriteLine("\n\n");
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
--- Geometri/Square.cs
+++ Geometri/Square.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace Geometri
 {
-    class Square
+    public class Square
     {
+        // Object attribute
+        private float side;
+
+        // Constructor
+        public float Side
+        {
+            get
+            {
+                return this.side;
+            }
+            set
+            {
+                this.side = value;
+            }
+        }
+
+        public Square(float a)
+        {
+            this.side = a;
+        }
+
+        public Square()
+        {
+            // Empty Constructor
+        }
+        public float Perimeter()
+        {
+            return side * 4;
+        }
+        public float Area()
+        {
+            return side * side;
+        }
     }
 }
